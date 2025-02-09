0394d7b9b5430043497f09422abc6c54b97d68ae
Gabriel <gabriel.jmaciel13@gmail.com>
2025-02-09 11:27:52 -0300

    Adicionar arquivos de projeto.

---
+++ exercício01/exercício01.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercício01
+{
+    internal class Exercício01
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
